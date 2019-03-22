a96fbe6009fef29d02e54c1041a78f2f8be8cf9c
halmarhalmar <46672234+halmarhalmar@users.noreply.github.com>
2019-03-22 10:01:48 +0700

    Delete hello.js

--- hello.js
+++
@@ -1,9 +0,0 @@
-var box
-main();
-function main(){
-	var box=document.createElement("textarea");
-	box.style.width="200px";
-	box.style.height="200px";
-	box.innerHTML=" Hello, Halmar 10215047";
-	document.body.append(box);
-}
