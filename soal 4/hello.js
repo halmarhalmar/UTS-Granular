071b96ca3e3c453cc71167ccc78d0e775ee6863a
halmarhalmar <46672234+halmarhalmar@users.noreply.github.com>
2019-03-22 10:03:00 +0700

    Update hello.js

--- soal 4/hello.js
+++ soal 4/hello.js
@@ -2,7 +2,7 @@ var box
 main();
 function main(){
 	var box=document.createElement("textarea");
-	box.style.width="200px";
+	box.style.width="2000px";
 	box.style.height="200px";
 	box.innerHTML=" Hello, Halmar yang ber-NIM 10215047!";
 	document.body.append(box);
