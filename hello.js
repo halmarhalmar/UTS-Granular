bd67c9f249539b36c1f8044d050e1d2f59c5300e
halmarhalmar <46672234+halmarhalmar@users.noreply.github.com>
2019-03-22 09:52:07 +0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -4,6 +4,6 @@ function main(){
 	var box=document.createElement("textarea");
 	box.style.width="200px";
 	box.style.height="200px";
-	box.innerHTML=" H E L L O  W O R L D ";
+	box.innerHTML=" Hello, Halmar 10215047";
 	document.body.append(box);
 }
